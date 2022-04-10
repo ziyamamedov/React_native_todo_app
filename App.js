157d0f6a876edfc40248c904fbce9ced343a22e3
mac <ziya_90@mail.ru>
2022-04-10 19:44:12 +0300

    Add Todo logic done

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Header } from './Components/Header';
 import { TodoForm } from './Components/TodoForm';
+import { TodoList } from './Components/TodoList';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+  const idRef = useRef(0)
+
   return (
-    <View style={styles.container}>
+    <View style={styles.appWrapper}>
       <Header />
-      <TodoForm />
+      <View style={styles.appContainer}>
+        <TodoForm setTodos={setTodos} idRef={idRef} />
+        <TodoList todos={todos} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appWrapper: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
   },
+  appContainer: {
+    width: '100%',
+    paddingHorizontal: '4%'
+  }
 });
---
+++ Components/TodoItem/index.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native"
+
+export const TodoItem = ({text}) => {
+
+  return (
+    <Text>{text}</Text>
+  )
+}
